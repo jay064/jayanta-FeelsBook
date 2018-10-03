e6cfab5a8c0209d77770d73defe9334836f5501a
jay064 <msagent619@gmail.com>
2018-10-03 17:33:33 -0600

    lab work commit

---
+++ app/src/main/java/com/example/jayanta_feelsbook/Emotion.java
@@ -0,0 +1,4 @@
+package com.example.jayanta_feelsbook;
+
+public abstract class Emotion{
+}
--- app/src/main/java/com/example/jayanta_feelsbook/MainActivity.java
+++ app/src/main/java/com/example/jayanta_feelsbook/MainActivity.java
@@ -4,12 +4,53 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String FILENAME = "file.sav";
+    private MainActivity activity = this;
+
+    private Button loveButton;
+    private Button joyButton;
+    private Button angerButton;
+    private Button sadnessButton;
+    private Button surpriseButton;
+    private Button fearButton;
+
+
+    private ArrayList<Emotion> emotions = new ArrayList<Emotion>();
+    private ArrayAdapter<Emotion> adapter;
+
+    public ListView getEmotionList() {
+        return EmotionList;
+    }
+
+    private ListView EmotionList;
+
+    public ArrayList<Emotion> getEmotions() {
+        return emotions;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        loveButton = (Button) findViewById(R.id.love);
+        fearButton = (Button) findViewById(R.id.fear);
+        sadnessButton = (Button) findViewById(R.id.sadness);
+        joyButton = (Button) findViewById(R.id.joy);
+        surpriseButton = (Button) findViewById(R.id.surprise);
+        angerButton = (Button) findViewById(R.id.anger);
+        EmotionList = (ListView) findViewById(R.id.EmotionList); //view
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
